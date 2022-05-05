32e06185eed6e55b01e28ec1e7337c599898328f
ksheer506 <ksheer506@naver.com>
2022-05-05 22:57:51 +0900

    Add files via upload

--- Accessibility/focus-highlight/focus-highlight.js
+++ Accessibility/focus-highlight/focus-highlight.js
@@ -1,14 +1,30 @@
-let focus = document.createElement("focus-highlight");
+const focus = document.createElement("focus-highlight");
+const initialStyles = {
+  position: "absolute",
+  border: "0px",
+  width: "0px",
+  height: "0px",
+  top: "0",
+  left: "0",
+  boxSizing: "border-box",
+  transition: "0.6s all",
+}
+
 document.body.appendChild(focus);
 
+for (const prop in initialStyles) {
+  focus.style[prop] = initialStyles[prop];
+}
+
 function showFocus(e) {
-  const currentEle = e.target;
+  const focusedEl = e.target;
+  const elementBound = focusedEl.getBoundingClientRect();
   const styles = {
-    left: `${currentEle.offsetLeft}px`,
-    top: `${currentEle.offsetTop}px`,
-    width: `${currentEle.offsetWidth}px`,
-    height: `${currentEle.offsetHeight}px`,
-    borderRadius: getComputedStyle(currentEle).borderRadius
+    left: `${elementBound.left}px`,
+    top: `${elementBound.top}px`,
+    width: `${focusedEl.offsetWidth}px`,
+    height: `${focusedEl.offsetHeight}px`,
+    borderRadius: getComputedStyle(focusedEl).borderRadius
   }
 
   focus.style.border = "2px solid rgb(228, 84, 108)";
@@ -29,16 +45,16 @@ function hideFocus(e) {
     focus.style[prop] = styles[prop];
   }
 }
-document.addEventListener("focusin", (e) => {
-  document.addEventListener("keyup", (e) => {
-    if (e.key === "Tab") {
-      showFocus(e)
+
+document.addEventListener("focusin", (e1) => {
+  document.addEventListener("keyup", (e2) => {
+    if (e2.key === "Tab") {
+      showFocus(e1)
     };
   })
 })
 
 window.addEventListener("blur", (e) => {
-  console.log(e);
   hideFocus(e);
 })
 
